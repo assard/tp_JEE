8c2783cd430b13676a4c74e4081c5e87ff6b5619
Axel_Chassard_ensg <axel.chassard@ensg.eu>
2021-01-08 15:48:18 +0100

    question 5 ok

--- tp_hibernate/src/main/java/tsi/ensg/jee/hibernate/ex1/Application.java
+++ tp_hibernate/src/main/java/tsi/ensg/jee/hibernate/ex1/Application.java
@@ -1,8 +1,19 @@
 package tsi.ensg.jee.hibernate.ex1;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class Application {
 
     public static void main(String[] args) {
+
+        SessionFactory sessionFactory = HibernateUtils.createSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(new Employee("Harry", "Potter", 1000));
+        session.getTransaction().commit();
+        session.close();
+
         System.out.println("Hello world !");
     }
 
