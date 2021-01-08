69e579659674b988cfbd423f07ad86e86f0005a6
Axel_Chassard_ensg <axel.chassard@ensg.eu>
2021-01-08 10:45:42 +0100

    annotation ok

--- tp/src/main/java/tsi/ensg/jee/tp2/exo2/Application.java
+++ tp/src/main/java/tsi/ensg/jee/tp2/exo2/Application.java
@@ -1,13 +1,15 @@
 package tsi.ensg.jee.tp2.exo2;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import tsi.ensg.jee.tp2.simplebis.Config;
 
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("config-onlineshopsimpler.xml");
-        Library library = (Library)context.getBean("library");
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigLibrary.class);
+        Library library = context.getBean(Library.class);
         System.out.println(library.toString());
     }
 
--- tp/src/main/java/tsi/ensg/jee/tp2/exo2/Library.java
+++ tp/src/main/java/tsi/ensg/jee/tp2/exo2/Library.java
@@ -1,11 +1,19 @@
 package tsi.ensg.jee.tp2.exo2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Library {
 
+    @Autowired
     private Set<Book> books;
+
+    public Library(){
+        this.books = new HashSet<Book>();
+    }
 
     public Library(Set<Book> books){
         this.books = books;
